db19085a8af019bdbed91d6ec06787ddd2ba5053
Zhang XiaoFei <1326336909@qq.com>
2021-09-02 11:17:38 +0800

    修复问题

--- app/Models/Api/Module/Education/Courseware/Point.php
+++ app/Models/Api/Module/Education/Courseware/Point.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models\Api\Module\Education\Courseware;
 
+use App\Models\Common\Module\Order\Courseware as OrderCourseware;
 use App\Models\Common\Module\Education\Courseware\Point as Common;
 
 /**
@@ -18,6 +19,49 @@ class Point extends Common
     'status',
     'update_time'
   ];
+
+  // 追加到模型数组表单的访问器
+  public $appends = [
+    'is_pay'
+  ];
+
+
+  /**
+   * @author zhangxiaofei [<1326336909@qq.com>]
+   * @dateTime 2021-06-25
+   * ------------------------------------------
+   * 是否购买课程封装
+   * ------------------------------------------
+   *
+   * 是否购买课程封装
+   *
+   * @param int $value 状态值
+   * @return 状态信息
+   */
+  public function getIsPayAttribute($value)
+  {
+    $member_id = auth('api')->user()->id ?? 0;
+
+    if(empty($member_id))
+    {
+      return false;
+    }
+
+    $where = [
+      'status'        => 1,
+      'member_id'     => $member_id,
+      'courseware_id' => $this->courseware_id
+    ];
+
+    $response = OrderCourseware::getRow($where);
+
+    if(empty($response->id))
+    {
+      return false;
+    }
+
+    return true;
+  }
 
 
 
